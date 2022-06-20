3279c6e97047602759040aaadb42cd07d1146008
Awsaf <awsaf49@gmail.com>
2022-06-20 21:05:45 +0600

    PUT/api

--- Week4/assignment2files/server.cpp
+++ Week4/assignment2files/server.cpp
@@ -32,7 +32,7 @@ void thread_to_recv(struct thread_data td)
     get_http_header_body(msg_recv, req_header, req_body);
 
     char * request = req_header;
-    char type[100], path[100], proto[100], name_part[100], email_part[100],  name[50], email[50], id_part[10];
+    char type[100], path[100], proto[100], name_part[100], email_part[100],  name[50], email[50], str_id[50], id_part[50];
 
     request = get_token(request, " ", type);
 
@@ -89,8 +89,8 @@ void thread_to_recv(struct thread_data td)
             {
                 char *path2 = path+1;
 //                printf(">> Path: %s\n",path2);
-                int found = read_html_file(path2, file_contents);
-                if(found)
+                int status = read_html_file(path2, file_contents);
+                if(status)
                 {
                     int con_len = strlen(file_contents);
                     sprintf(response, "HTTP/1.1 200 OK\nContent-length:%d\n\n%s",con_len,file_contents);
@@ -135,6 +135,40 @@ void thread_to_recv(struct thread_data td)
 
         }
 
+    }
+    else if(strcmp(type, "PUT")==0)
+    {
+        if(strcmp(path,"/api")==0)
+        {
+            char * p = req_body;
+            p = get_token(p, "&", id_part);
+            p = get_token(p, "&", name_part);
+            p = get_token(p, "&", email_part);
+
+            p = id_part;
+            p = get_token(p, "=", str_id);
+            p = get_token(p, "=", str_id); // second token
+            int id = atoi(str_id);
+
+            p = name_part;
+            p = get_token(p, "=", name);
+            p = get_token(p, "=", name); //second token
+
+            p = email_part;
+            p = get_token(p, "=", email);
+            p = get_token(p, "=", email); //second token
+
+            printf("Id = %d | Name = %s | email = %s\n",id, name, email);
+            int status = update_record(id, name, email);
+            if(status)
+            {
+                sprintf(response, "HTTP/1.1 204 NO CONTENT\nContent-length:0");
+            }
+            else
+            {
+                sprintf(response, "HTTP/1.1 404 NOT FOUND\nContent-length:0");
+            }
+        }
     }
 
     bool success = send_to_socket(s, response);
